7d526e9c308176fd3f2c35d2d683df96d2b05894
liaozhongkai <LZK1997714>
2020-11-23 11:07:10 +0800

    feat: 添加进度调节按钮

--- customhorizontalbar/src/main/java/com/lzk/customhorizontalbar/MainActivity.java
+++ customhorizontalbar/src/main/java/com/lzk/customhorizontalbar/MainActivity.java
@@ -6,13 +6,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private HorizontalBar mHorizontalBar;
     private TagHorizontalBar mTagHorizontalBar;
+    private Button mMainRandomBtn;
     private int value = 0;
     private int value1 = 0;
+    private Random mRandom = new Random();
+
     private Handler mHandler = new Handler(Looper.myLooper()){
         @Override
         public void handleMessage(Message msg) {
@@ -41,8 +48,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mHorizontalBar = findViewById(R.id.main_hb);
-        mHandler.sendEmptyMessageDelayed(0,1*1000);
+        //mHandler.sendEmptyMessageDelayed(0,1*1000);
         mTagHorizontalBar = findViewById(R.id.main_tag_hb);
-        mHandler.sendEmptyMessageDelayed(1,1000);
+       // mHandler.sendEmptyMessageDelayed(1,1000);
+
+        mMainRandomBtn = findViewById(R.id.main_random_btn);
+
+        mMainRandomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int progress = mRandom.nextInt(100);
+                mHorizontalBar.setUI(progress,100,true);
+                mTagHorizontalBar.setUI(progress,100,"同比增长");
+            }
+        });
     }
 }
